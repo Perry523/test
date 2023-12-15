56f8c073c818148438c5b6862c97a212224ee8b0
perry523 <wendel.renato@gmail.com>
2023-12-15 04:02:07 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="notify">teste</button>
+  </div>
+</template>
+<script setup lang="ts">
+function notify() {
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      setTimeout(() => {
+        new Notification('Hello, world!')
+      }, 5000)
+    }
+  })
+}
+</script>
