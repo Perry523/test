163e9e460e691494f74a41dc8ae3d3e0a6ff7911
perry523 <wendel.renato@gmail.com>
2023-12-15 04:27:41 -0300

    teste diferentes versoes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
     <button @click="notify">teste</button>
-
+    <button @click="notify2">teste2</button>
   </div>
 </template>
-<script setup lang="ts">
+<script setup>
 navigator.serviceWorker.register("sw.js");
 
 function notify() {
   Notification.requestPermission().then((result) => {
     if (result === "granted") {
       navigator.serviceWorker.ready.then((registration) => {
-        registration.showNotification("Vibration Sample", {
+        registration.showNotification(Math.random(200), {
           body: "Buzz! Buzz!",
           icon: "../images/touch/chrome-touch-icon-192x192.png",
           vibrate: [200, 100, 200, 100, 200, 100, 200],
           tag: "vibration-sample",
         });
+      });
+    }
+  });
+}
+
+function notify2() {
+  Notification.requestPermission().then((result) => {
+    if (result === "granted") {
+      new Notification("teste", {
+        body: "Buzz! Buzz 2!",
+        icon: "../images/touch/chrome-touch-icon-192x192.png",
+        vibrate: [200, 100, 200, 100, 200, 100, 200],
+        tag: "vibration-sample",
       });
     }
   });
