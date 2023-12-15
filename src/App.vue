8f3bc4593a247e37d0b60d010a034372e2f71c78
perry523 <wendel.renato@gmail.com>
2023-12-15 04:29:47 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@
     <button @click="notify2">teste2</button>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 navigator.serviceWorker.register("sw.js");
 
 function notify() {
   Notification.requestPermission().then((result) => {
     if (result === "granted") {
       navigator.serviceWorker.ready.then((registration) => {
+        // @ts-ignore
         registration.showNotification(Math.random(200), {
           body: "Buzz! Buzz!",
           icon: "../images/touch/chrome-touch-icon-192x192.png",
