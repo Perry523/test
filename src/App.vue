9fe03ea53ef032a14481cf6db94b8dc5da55ee4c
perry523 <wendel.renato@gmail.com>
2023-12-15 04:21:41 -0300

    pwa

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <button @click="notify">teste</button>
-    
+
   </div>
 </template>
 <script setup lang="ts">
+navigator.serviceWorker.register("sw.js");
+
 function notify() {
-  Notification.requestPermission().then((permission) => {
-    if (permission === 'granted') {
-      new Notification('Hello, world!')
+  Notification.requestPermission().then((result) => {
+    if (result === "granted") {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification("Vibration Sample", {
+          body: "Buzz! Buzz!",
+          icon: "../images/touch/chrome-touch-icon-192x192.png",
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: "vibration-sample",
+        });
+      });
     }
-  })
+  });
 }
 </script>
