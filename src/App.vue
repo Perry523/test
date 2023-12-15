3bc1da2d6d154325b28c2ccf8127344ec691e56e
perry523 <wendel.renato@gmail.com>
2023-12-15 04:11:06 -0300

    change

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
     <button @click="notify">teste</button>
+    
   </div>
 </template>
 <script setup lang="ts">
 function notify() {
   Notification.requestPermission().then((permission) => {
     if (permission === 'granted') {
-      setTimeout(() => {
-        new Notification('Hello, world!')
-      }, 5000)
+      new Notification('Hello, world!')
     }
   })
 }
